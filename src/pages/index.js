e29a4ccb7c9c5205748b225fa16b40fbdbcf9487
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-14 12:40:18 +0500

    form ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
+import { useRef } from 'react';
 export default function Home() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const feedback = feedbackRef.current.value;
+  };
   return (
     <>
       <h1>This is Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input id="email" type="email" ref={emailRef}></input>
+        </div>
+
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </>
   );
 }
