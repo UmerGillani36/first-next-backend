8fc5dac356d60920c1ba2f9ed5844ad2ce7d099f
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-14 12:58:16 +0500

    first POST api for feedback

--- src/pages/api/feedback.js
+++ src/pages/api/feedback.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
 function handler(req, res) {
-  res.status(200).json({ message: 'This Works' });
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: 'Success!', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'This Works' });
+  }
 }
 
 export default handler;
