66af69ddb079d636dcb3bc583a46f99ee8bcfe8c
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-19 14:58:05 +0500

    get feedback backend

--- src/pages/api/feedback.js
+++ src/pages/api/feedback.js
@@ -1,5 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+function getPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+function readData(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
 function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
@@ -10,14 +18,15 @@ function handler(req, res) {
       email: email,
       text: feedbackText,
     };
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = getPath();
+    const data = readData(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: 'Success!', feedback: newFeedback });
   } else {
-    res.status(200).json({ message: 'This Works' });
+    const filePath = getPath();
+    const data = readData(filePath);
+    res.status(200).json({ feedback: data });
   }
 }
 
